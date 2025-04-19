0b06cebbae71cf6246e7efdbd2560e941ae17f0b
Chevee Ferrer <cferrerdesign@gmail.com>
2025-04-19 16:40:10 +0800

    added export on constant

--- src/shared/constants.jsx
+++ src/shared/constants.jsx
@@ -1,2 +1,2 @@
 // export const API_URL = "http://localhost:5000";
-const API_URL = "https://gardenia-ocr-prototype-1.onrender.com";
+export const API_URL = "https://gardenia-ocr-prototype-1.onrender.com";
