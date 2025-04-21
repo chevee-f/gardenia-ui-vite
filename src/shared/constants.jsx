8498baa1edad340fe49c75cd2344ace54bba7d16
Chevee Ferrer <cferrerdesign@gmail.com>
2025-04-21 15:52:12 +0800

    updated cnstant

--- src/shared/constants.jsx
+++ src/shared/constants.jsx
@@ -1,2 +1,2 @@
 // export const API_URL = "http://localhost:5000";
-export const API_URL = "https://gardenia-ocr-prototype-1.onrender.com";
+export const API_URL = "https://gardenia-ocr-prototype-2.onrender.com";
