ca2de7a231621ba78b337ff6fc2337d2409a619b
Chevee Ferrer <cferrerdesign@gmail.com>
2026-02-25 16:08:35 +0800

    updated email send to admins only

--- src/constants/email.js
+++ src/constants/email.js
@@ -1,3 +1,3 @@
 // Billing email recipients (can be a single email string or an array of email strings)
-export const SPARE_PARTS_BILLING_EMAIL = ["chevee.kid@gmail.com", "chevpk26@gmail.com"];
-export const CYKRIS_BILLING_EMAIL = ["chevee.kid@gmail.com", "chevpk26@gmail.com"];
+export const SPARE_PARTS_BILLING_EMAIL = ["daisygracesanama0593@yahoo.com", "ervycustomsbrokerage@yahoo.com.ph"];
+export const CYKRIS_BILLING_EMAIL = ["daisygracesanama0593@yahoo.com", "ervycustomsbrokerage@yahoo.com.ph"];
