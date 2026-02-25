bedf74e6d07662a0f92b2ac50f05a5a1b5d2336a
Chevee Ferrer <cferrerdesign@gmail.com>
2026-02-25 16:33:55 +0800

    added more emails

--- src/constants/email.js
+++ src/constants/email.js
@@ -1,3 +1,15 @@
 // Billing email recipients (can be a single email string or an array of email strings)
-export const SPARE_PARTS_BILLING_EMAIL = ["daisygracesanama0593@yahoo.com", "ervycustomsbrokerage@yahoo.com.ph"];
-export const CYKRIS_BILLING_EMAIL = ["daisygracesanama0593@yahoo.com", "ervycustomsbrokerage@yahoo.com.ph"];
+export const SPARE_PARTS_BILLING_EMAIL = [
+    "daisygracesanama0593@yahoo.com", 
+    "ervycustomsbrokerage@yahoo.com.ph",
+    "acctng.ervylogistics@gmail.com",
+    "aileenmatub2015@gmail.com",
+    "marjoracion30@yahoo.com",
+];
+export const CYKRIS_BILLING_EMAIL = [
+    "daisygracesanama0593@yahoo.com", 
+    "ervycustomsbrokerage@yahoo.com.ph",
+    "acctng.ervylogistics@gmail.com",
+    "aileenmatub2015@gmail.com",
+    "marjoracion30@yahoo.com",
+];
