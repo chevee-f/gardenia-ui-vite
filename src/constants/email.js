e295b3536098ad7fc930f06211b5f4600948d216
Chevee Ferrer <cferrerdesign@gmail.com>
2026-02-26 15:30:28 +0800

    fixed wrong email added

--- src/constants/email.js
+++ src/constants/email.js
@@ -1,15 +1,23 @@
 // Billing email recipients (can be a single email string or an array of email strings)
+// export const SPARE_PARTS_BILLING_EMAIL = [
+//     "chevee.kid@gmail.com",
+//     "cferrerdesign@gmail.com",
+// ];
+// export const CYKRIS_BILLING_EMAIL = [
+//     "chevee.kid@gmail.com",
+//     "cferrerdesign@gmail.com",
+// ];
 export const SPARE_PARTS_BILLING_EMAIL = [
     "daisygracesanama0593@yahoo.com", 
     "ervycustomsbrokerage@yahoo.com.ph",
-    "acctng.ervylogistics@gmail.com",
+    "acctg.ervylogistics@gmail.com",
     "aileenmatub2015@gmail.com",
     "marjoracion30@yahoo.com",
 ];
 export const CYKRIS_BILLING_EMAIL = [
     "daisygracesanama0593@yahoo.com", 
     "ervycustomsbrokerage@yahoo.com.ph",
-    "acctng.ervylogistics@gmail.com",
+    "acctg.ervylogistics@gmail.com",
     "aileenmatub2015@gmail.com",
     "marjoracion30@yahoo.com",
 ];
